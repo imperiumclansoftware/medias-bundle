169e07ae019f0d30a75867dcf6a6ad400a6937b4
David Dutas <david.dutas@gmail.com>
2023-01-12 14:17:53 +0100

    Correct ini files read error

--- src/Service/MediaService.php
+++ src/Service/MediaService.php
@@ -5,6 +5,7 @@ namespace ICS\MediaBundle\Service;
 use Symfony\Component\HttpKernel\KernelInterface;
 use ICS\MediaBundle\Entity\MediaFile;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 
 class MediaService
 {
@@ -39,19 +40,25 @@ class MediaService
         $post_overhead = 1024; // POST data contains more than just the file upload; see comment from @jlh
         $files = array_merge(array(php_ini_loaded_file()), explode(",\n", php_ini_scanned_files()));
         foreach (array_filter($files) as $file) {
-            $ini = parse_ini_file($file);
-            $regex = '/^([0-9]+)([bkmgtpezy])$/i';
-            if (!empty($ini['post_max_size']) && preg_match($regex, $ini['post_max_size'], $match)) {
-                $post_max_size = round($match[1] * pow(1024, stripos('bkmgtpezy', strtolower($match[2]))));
-                if ($post_max_size > 0) {
-                    $max_size = $post_max_size - $post_overhead;
+            try {
+                $ini = parse_ini_file($file,true);
+                $regex = '/^([0-9]+)([bkmgtpezy])$/i';
+                if (!empty($ini['post_max_size']) && preg_match($regex, $ini['post_max_size'], $match)) {
+                    $post_max_size = round($match[1] * pow(1024, stripos('bkmgtpezy', strtolower($match[2]))));
+                    if ($post_max_size > 0) {
+                        $max_size = $post_max_size - $post_overhead;
+                    }
+                }
+                if (!empty($ini['upload_max_filesize']) && preg_match($regex, $ini['upload_max_filesize'], $match)) {
+                    $upload_max_filesize = round($match[1] * pow(1024, stripos('bkmgtpezy', strtolower($match[2]))));
+                    if ($upload_max_filesize > 0 && ($max_size <= 0 || $max_size > $upload_max_filesize)) {
+                        $max_size = $upload_max_filesize;
+                    }
                 }
             }
-            if (!empty($ini['upload_max_filesize']) && preg_match($regex, $ini['upload_max_filesize'], $match)) {
-                $upload_max_filesize = round($match[1] * pow(1024, stripos('bkmgtpezy', strtolower($match[2]))));
-                if ($upload_max_filesize > 0 && ($max_size <= 0 || $max_size > $upload_max_filesize)) {
-                    $max_size = $upload_max_filesize;
-                }
+            catch(Exception $ex)
+            {
+
             }
         }
 
